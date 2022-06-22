e00dd6bacea3671ce5854241b75573c872d708b5
route-2 <ruthurao@gmail.com>
2022-06-22 19:45:41 +0530

    carousel

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import LoginButton from './modules/auth/components/LoginButton';
 import Navbar from './common/components/Navbar';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+     <Homepage/>
     </div>
   );
 }
---
+++ src/pages/Homepage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../common/components/Navbar";
+
+import Carousel from "../components/Carousel";
+
+const Homepage = () => {
+    return (
+        <>
+      <Navbar/>
+      <Carousel/>
+
+
+
+
+
+
+
+        </>
+    )
+}
+export default Homepage
