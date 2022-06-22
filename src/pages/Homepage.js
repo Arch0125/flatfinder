d8b9ce96ecdb86b5bc9a89c555de9c7dc78580af
route-2 <ruthurao@gmail.com>
2022-06-22 16:16:48 +0530

    update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
+import Search from "../components/SearchBar";
 
 const Home = () => {
     return (
         <>
-        
+
             <Navbar/>
             <Carousel/>
+            <Search/>
            
         </>
     )
