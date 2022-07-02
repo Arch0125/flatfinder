575683092a29bd0b9451871edd764f305722ba38
route-2 <ruthurao@gmail.com>
2022-07-02 21:49:49 +0530

    update

--- src/modules/auth/components/LandingPage.jsx
+++ src/modules/auth/components/LandingPage.jsx
@@ -7,7 +7,7 @@ const Land=()=>{
 <Navbar/>
 <div className="w-full h-96 bg-gradient-to-r from-indigo-400 via-purple-300 to-pink-200 ">
 <div> 
-<h1 className="text-white font-bold text-6xl pt-12   "> Explore Flats with Us </h1>
+<h1 className="text-white font-bold text-6xl pt-12 pb-8   "> Explore Flats with Us </h1>
 </div>
 <Carousel/>
 
