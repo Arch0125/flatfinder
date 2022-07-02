cd83182043b0cfa7a3c6b4feede572c6b5601642
route-2 <ruthurao@gmail.com>
2022-07-03 00:36:42 +0530

    update

--- src/common/components/Navbar.jsx
+++ src/common/components/Navbar.jsx
@@ -4,9 +4,12 @@ import LoginButton from '../../modules/auth/components/LoginButton'
 const Navbar = () => {
   return (
     <div className=' flex items-center flex-row justify-between w-[100vw] h-fit-content] shadow-xl bg-black p-4 '>
+     
       <label className='text-2xl font-semibold text-white '>
+      <img className='w-10 remove.bg' src="https://i.ibb.co/qdmXjWF/uruthu.png" />
         flatfinder
       </label>
+     
       <LoginButton />
     </div>
   )
